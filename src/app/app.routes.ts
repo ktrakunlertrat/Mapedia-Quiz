c266e114475b5307e242c293e5f89f6cbc3a7ad6
LAPTOP-CN4HVAQM\W10H-X64 <ktrakunlertrat@gmail.com>
2025-07-25 10:13:39 +0700

    จัดระเบียบโค้ด

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './layouts/main-layout';
-import { Home } from './home/home';
 import { Detail } from './detail/detail';
 
 export const routes: Routes = [
   {
     path: '',
-    component: MainLayout,
-    children: [
-      { path: '', component: Home }
-    ]
+    component: MainLayout
   },
   {
     path: 'detail',
